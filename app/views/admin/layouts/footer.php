0ddf75da5653840f4a96dad20bf733854a477ff7
AZinkey <azinkey@gmail.com>
2014-07-07 03:22:20 +0530

    Fix bootbox pop on remove

--- app/views/admin/layouts/footer.php
+++ app/views/admin/layouts/footer.php
@@ -9,4 +9,5 @@
 
 <script src="<?= skin_url(); ?>scripts/jquery.cookie.js" type="text/javascript"></script>
 <script src="<?= skin_url(); ?>scripts/jquery.slimscroll.min.js" type="text/javascript"></script>
+<script src="<?= skin_url(); ?>scripts/bootbox.js" type="text/javascript"></script>
 <script src="<?= skin_url(); ?>scripts/theme.js" type="text/javascript"></script>
