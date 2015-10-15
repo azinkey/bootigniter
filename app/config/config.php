69abdd22594e709b1ad477d84a46206b6def71d4
azinkey <azinkey@gmail.com>
2015-10-15 08:01:54 -0700

    Set Session Expired configuration for 15 mnt

--- app/config/config.php
+++ app/config/config.php
@@ -246,7 +246,7 @@ $config['encryption_key'] = 'Fxj<k+A(Ud*HR)Q[X4<E_mNjU|]DuyS$[1i0,nCSH3i<dF5-Y+c
 |
 */
 $config['sess_cookie_name']		= 'ci_session';
-$config['sess_expiration']		= 7200;
+$config['sess_expiration']		= 900;
 $config['sess_expire_on_close']         = FALSE;
 $config['sess_encrypt_cookie']          = FALSE;
 $config['sess_use_database']            = FALSE;
