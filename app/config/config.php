f5c672d2de3994af9b3817765c269fc0b641c313
Suraj Mishra <azinkey@gmail.com>
2017-01-04 21:56:43 +0530

    Update Codeigniter version 3.1.2

--- app/config/config.php
+++ app/config/config.php
@@ -16,7 +16,7 @@
 | environments.
 |
 */
-$config['base_url'] = '';
+$config['base_url'] = 'http://localhost/bootigniter/';
 
 /*
 |--------------------------------------------------------------------------
