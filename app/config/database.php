09989802d0e414dfb272b1b780149101fccdce27
Suraj Mishra <suraj.mishra@kadamtech.com>
2017-01-30 11:26:42 +0530

    set default database setting

--- app/config/database.php
+++ app/config/database.php
@@ -50,7 +50,7 @@ $active_record = TRUE;
 
 $db['default']['hostname'] = 'localhost'; 	// update your database host
 $db['default']['username'] = 'root'; 		// update database username
-$db['default']['password'] = 'kadam@123'; 			// put your database password
+$db['default']['password'] = ''; 			// put your database password
 $db['default']['database'] = 'bootigniter'; // Change your database namne
 $db['default']['dbdriver'] = 'mysqli';
 $db['default']['dbprefix'] = 'az_';
